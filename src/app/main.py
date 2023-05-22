23b01a15d752089e6678ffef44f0738df92235fb
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-22 21:39:48 +0300

    register questions router

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from core.settings import get_settings
+from api.endpoints import router as questions_router
 
 settings = get_settings()
 
 app = FastAPI(title=settings.PROJECT_NAME)
+app.include_router(questions_router, tags=["questions"])
