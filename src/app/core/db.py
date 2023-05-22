3a9a9c1dae3eb5524c6512d6b8ea434973731e07
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-22 19:47:07 +0300

    add db.py

---
+++ src/app/core/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from .settings import get_settings
+
+settings = get_settings()
+
+
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+
+async def get_session() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
