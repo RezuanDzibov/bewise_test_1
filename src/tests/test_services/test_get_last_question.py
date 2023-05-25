e8d57fcebb4f985bf11c51f29ebac2705e0d7702
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-25 17:10:01 +0300

    add test_get_last_question.py

--- src/tests/test_services/test_get_last_question.py
+++ src/tests/test_services/test_get_last_question.py
@@ -4,6 +4,10 @@ from schemas import QuestionSchema
 from services import get_last_question
 
 
-async def test_success(session: AsyncSession, last_question_in_db: QuestionSchema):
+async def test_questions_exist(session: AsyncSession, last_question_in_db: QuestionSchema):
     last_question = await get_last_question(session)
     assert last_question.id == last_question_in_db.at_api_id
+
+
+async def test_questions_not_exist(session: AsyncSession):
+    assert not await get_last_question(session)
